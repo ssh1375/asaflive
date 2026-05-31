3f69496da42f3bcfc3eb528f2591ec7867b4c7f2
sajad <hendeseh20@gmail.com>
2026-05-31 23:48:49 +0330

    adding adapter to primsa in service

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+    constructor() {
+        const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+        const adapter = new PrismaPg(pool);
+        super({ adapter });
+    }
+
+    async onModuleInit() {
+        await this.$connect();
+        console.log('Database connected successfully');
+    }
+}
