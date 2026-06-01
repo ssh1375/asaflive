fcacb2e4c4bdf8d7257fa22baf7697a638fe7562
sajad <hendeseh20@gmail.com>
2026-06-01 11:43:03 +0330

    user auth role permission module has been created

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { RbacModule } from './rbac/rbac.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [PrismaModule, ConfigModule.forRoot({
     isGlobal: true
-  })],
+  }), RbacModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
-import { Prisma } from 'generated/prisma/browser';
+
 import { User } from 'generated/prisma/client'
 
 @Injectable()
 export class AppService {
   constructor(private prisma: PrismaService){}
   async getHello(): Promise<User> {
+   
     const user = await this.prisma.user.create({
       data: {
         email: 'hendeseh20@gmail.com',
@@ -14,5 +15,6 @@ export class AppService {
       }
     });
     return user;
+    
   }
 }
