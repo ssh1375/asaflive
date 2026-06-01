fc3ba9b7ead1c58e6cc514212ad221c5ecf5c6c3
sajad <hendeseh20@gmail.com>
2026-06-01 10:48:59 +0330

    correct docker postgres yml

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { User } from 'generated/prisma/client';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello(): Promise<User> {
+    return await this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
+import { Prisma } from 'generated/prisma/browser';
+import { User } from 'generated/prisma/client'
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService){}
+  async getHello(): Promise<User> {
+    const user = await this.prisma.user.create({
+      data: {
+        email: 'hendeseh20@gmail.com',
+        name: "sajad"
+      }
+    });
+    return user;
   }
 }
